495198c152ab186fc6203408bf1ce71ef10802ed
Ivan Mercedes <ivanmercede@gmail.com>
2024-09-04 17:21:25 -0400

    feat: add pie chart

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,29 @@
+import Colors from "@/constants/Colors";
 import { ScrollView, Text, View } from "react-native";
+import { PieChart } from "react-native-gifted-charts";
 
 const page = () => {
+
+  const pieData = [
+    {
+      value: 47,
+      color: Colors.tintColor,
+      focused: true,
+      text: "47%",
+    },
+    {
+      value: 40,
+      color: Colors.blue,
+      text: "40%",
+    },
+    {
+      value: 16,
+      color: Colors.white,
+      text: "16%",
+    },
+    { value: 3, color: "#FFA5BA", gradientCenterColor: "#FF7F97", text: "3%" },
+  ];
+
   return (
     <View className="flex-1 bg-black pt-3 px-5">
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -13,8 +36,28 @@ const page = () => {
               $2700.<Text className="font-normal text-lg">00</Text>
             </Text>
           </View>
-          <View>
-            <Text className="text-white">Pie chart</Text>
+          <View  className="py-5 items-center">
+          <PieChart
+          data={pieData}
+          donut
+          showGradient
+          sectionAutoFocus
+          semiCircle
+          focusOnPress
+          radius={70}
+          innerRadius={55}
+          innerCircleColor={Colors.black}
+          centerLabelComponent={() => {
+            return (
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <Text
+                  style={{fontSize: 22, color: 'white', fontWeight: 'bold'}}>
+                  47%
+                </Text>
+              </View>
+            );
+          }}
+        />
           </View>
         </View>
       </ScrollView>

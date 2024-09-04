603f3394ceb0379df41de39505c16df4e989aadb
Ivan Mercedes <ivanmercede@gmail.com>
2024-09-04 17:05:17 -0400

    add react native chart library & index home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,24 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 const page = () => {
   return (
-    <View className="flex-1 bg-black py-10">
-      <Text className="text-white">index</Text>
-    </View> 
+    <View className="flex-1 bg-black pt-3 px-5">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="flex flex-row justify-between items-center">
+          <View className="gap-2">
+            <Text className="text-white text-base">
+              My <Text className="font-bold">Expenses</Text>
+            </Text>
+            <Text className="text-2xl text-white font-bold">
+              $2700.<Text className="font-normal text-lg">00</Text>
+            </Text>
+          </View>
+          <View>
+            <Text className="text-white">Pie chart</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
